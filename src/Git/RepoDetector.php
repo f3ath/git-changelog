c672d7789c27212e526c47a09f4ba43fc3b4854d
Sergey Protko <fesors@gmail.com>
2016-12-25 16:00:41 +0300

    Simplify repo detector

--- src/Git/RepoDetector.php
+++ src/Git/RepoDetector.php
@@ -3,15 +3,14 @@ namespace F3\Changelog\Git;
 
 class RepoDetector
 {
-    public function getDiffUrl(string $remote_url, string $from, $to): string
+    public function getDiffUrl(string $remoteUrl, string $from, $to): string
     {
-        if (
-            preg_match('#^git@github.com:(?P<org>[\w_-]+)/(?P<repo>[\w_-]+).git$#', $remote_url, $m)
-            || preg_match('#^https://github.com/(?P<org>[\w_-]+)/(?P<repo>[\w_-]+)(.git)?$#', $remote_url, $m)
-        ) {
+        $regExp = '#^(git@github.com:|https://github.com/)(?P<org>[\w_-]+)/(?P<repo>[\w_-]+).git$#';
+
+        if (preg_match($regExp, $remoteUrl, $m)) {
             return "https://github.com/{$m['org']}/{$m['repo']}/compare/{$from}...{$to}";
-        } else {
-            throw new \UnexpectedValueException("Can not parse remote: $remote_url. ");
         }
+
+        throw new \UnexpectedValueException("Can not parse remote: $remoteUrl. ");
     }
 }
--- test/Git/RepoDetectorTest.php
+++ test/Git/RepoDetectorTest.php
@@ -3,13 +3,54 @@ namespace F3\Changelog\Git;
 
 class RepoDetectorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGithub()
+    private $detector;
+
+    public function setUp()
     {
-        $detector = new RepoDetector();
+        $this->detector = new RepoDetector();
+    }
+
+    /**
+     * @dataProvider positiveCasesProvider
+     */
+    public function test_positive_cases_for_url_parsing($repoUrl, $tags, $result)
+    {
         $this->assertEquals(
-            'https://github.com/foo/bar/compare/a...b',
-            $detector->getDiffUrl('https://github.com/foo/bar.git', 'a', 'b')
+            $result,
+            $this->detector->getDiffUrl($repoUrl, ...$tags)
         );
     }
 
+    /**
+     * @dataProvider negativeCasesProvider
+     * @expectedException \UnexpectedValueException
+     */
+    public function test_negative_cases_for_url_parsing($url)
+    {
+        $this->detector->getDiffUrl($url, 'a', 'b');
+    }
+
+    public function positiveCasesProvider()
+    {
+        return [
+            [
+                'https://github.com/foo/bar.git',
+                ['a', 'b'],
+                'https://github.com/foo/bar/compare/a...b'
+            ],
+            [
+                'git@github.com:foo/bar.git',
+                ['a', 'b'],
+                'https://github.com/foo/bar/compare/a...b'
+            ]
+        ];
+    }
+
+    public function negativeCasesProvider()
+    {
+        return [
+            ['git@gitlab.com:fail'],
+            ['https://google.com']
+        ];
+    }
 }
