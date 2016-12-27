794919698282b98995336a2e9ff43f085d7922cf
Alexey Karapetov <karapetov@gmail.com>
2016-12-26 23:17:53 -0800

    RepoDetector tests

--- test/Git/RepoDetectorTest.php
+++ test/Git/RepoDetectorTest.php
@@ -3,12 +3,42 @@ namespace F3\Changelog\Git;
 
 class RepoDetectorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGithub()
+    public function urlDataProvider(): array
+    {
+        return [
+            [
+                'https://github.com/foo/bar/compare/a...b',
+                'https://github.com/foo/bar.git',
+            ],
+            [
+                'https://github.com/foo/bar/compare/a...b',
+                'https://github.com/foo/bar',
+            ],
+            [
+                'https://github.com/f3ath/git-changelog/compare/a...b',
+                'git@github.com:f3ath/git-changelog.git',
+            ],
+        ];
+    }
+
+    /**
+     * @param string $expected
+     * @param string $url
+
+     * @dataProvider urlDataProvider
+     */
+    public function testGithub(string $expected, string $url)
     {
         $detector = new RepoDetector();
-        $this->assertEquals(
-            'https://github.com/foo/bar/compare/a...b',
-            $detector->getDiffUrl('https://github.com/foo/bar.git', 'a', 'b')
-        );
+        $this->assertEquals($expected, $detector->getDiffUrl($url, 'a', 'b'));
+    }
+
+    /**
+     * @expectedException \UnexpectedValueException
+     */
+    public function testInvalidUrl()
+    {
+        $detector = new RepoDetector();
+        $detector->getDiffUrl('https://google.com', 'a', 'b');
     }
 }
